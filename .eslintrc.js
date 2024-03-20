d85a0e6c6e82deabb2757a37a52c0fce2327c2ed
Hong <917777904@qq.com>
2024-03-20 23:46:08 +0800

    初始化项目

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,22 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    'plugin:prettier/recommended'
   ],
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: '@babel/eslint-parser'
   },
+  // 0,off: 忽略，1,warn: 警告，2,error: 报错
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    semi: ['error', 'never'], // 不允许将分号作为语句的结尾
+    eqeqeq: 'warn', // 使用全等
+    'comma-dangle': ['error', 'never'] // 对象字面量项尾不能有逗号
+  }
+}
