a2504e67f354180943d31771ed02103b44fbabf8
Hong <917777904@qq.com>
2024-03-31 21:56:29 +0800

    利用vuex存储主题

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,6 @@
 import { createStore } from 'vuex'
+import app from './modules/app'
 
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  modules: { app }
 })
