81bea2d59fece81a64ab8800c0a961c5db738629
Hong <917777904@qq.com>
2024-04-06 23:14:23 +0800

    修改项目名

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/mytab/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/browsertab/' : '/',
   devServer: {
     port: 9000
   }
